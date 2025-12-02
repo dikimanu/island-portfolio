7b641a2b2e0dadb9017286ea53ac3a9c58b82479
dikimanu <sp20060924@gmail.com>
2025-12-02 20:29:16 +0530

    Update EarthCanvas.jsx

--- src/models/EarthCanvas.jsx
+++ src/models/EarthCanvas.jsx
@@ -1,19 +1,17 @@
-import React, { Suspense, useMemo, useRef } from "react";
+import React, { Suspense } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF, Points, PointMaterial } from "@react-three/drei";
 import * as random from "maath/random/dist/maath-random.esm";
 import Loader from "../components/Loader";
 
-// Starfield Component
-const Stars = () => {
-  const ref = useRef();
-
-  const sphere = useMemo(
-    () => random.inSphere(new Float32Array(5000), { radius: 1.2 }),
-    []
+// Stars component
+const Stars = (props) => {
+  const ref = React.useRef();
+  const [sphere] = React.useState(() =>
+    random.inSphere(new Float32Array(5000), { radius: 1.2 })
   );
 
-  useFrame((_, delta) => {
+  useFrame((state, delta) => {
     if (ref.current) {
       ref.current.rotation.x -= delta / 10;
       ref.current.rotation.y -= delta / 15;
@@ -22,7 +20,7 @@ const Stars = () => {
 
   return (
     <group rotation={[0, 0, Math.PI / 4]}>
-      <Points ref={ref} positions={sphere} stride={3} frustumCulled>
+      <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
         <PointMaterial
           transparent
           color="#f272c8"
@@ -35,47 +33,38 @@ const Stars = () => {
   );
 };
 
-// Earth GLTF Model
-const EarthModel = () => {
+// Earth model
+const EarthModel = (props) => {
   const { scene } = useGLTF("/planet/scene.gltf");
-  return <primitive object={scene} scale={2} />;
+  return <primitive object={scene} scale={2.2} {...props} />;
 };
 
-// Main Canvas
-export default function EarthCanvas() {
+// Main Canvas: Stars + Earth
+const EarthCanvas = () => {
   return (
-    <div
-      id="earth-wrapper"
-      style={{
-        position: "fixed",
-        top: 0,
-        left: 0,
-        width: "100vw",
-        height: "100vh",
-        overflow: "hidden",
-      }}
+    <Canvas
+      shadows
+      frameloop="demand"
+      dpr={[1, 2]}
+      gl={{ preserveDrawingBuffer: true }}
+      camera={{ fov: 45, near: 0.1, far: 200, position: [0, 0, 5] }}
     >
-      <Canvas
-        shadows
-        dpr={[1, 2]}
-        gl={{ preserveDrawingBuffer: true }}
-        camera={{ fov: 45, near: 0.1, far: 200, position: [0, 0, 5] }}
-      >
-        <ambientLight intensity={1} />
-        <directionalLight position={[5, 3, 5]} intensity={2.5} />
+      <ambientLight intensity={1} />
+      <directionalLight position={[5, 3, 5]} intensity={2.5} />
 
-        <Suspense fallback={<Loader />}>
-          <OrbitControls
-            autoRotate
-            enableZoom={false}
-            maxPolarAngle={Math.PI / 2}
-            minPolarAngle={Math.PI / 2}
-          />
-          <Stars />
-          <EarthModel />
-          <Preload all />
-        </Suspense>
-      </Canvas>
-    </div>
+      <Suspense fallback={<Loader />}>
+        <OrbitControls
+          autoRotate
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Stars scale={5} />
+        <EarthModel />
+        <Preload all />
+      </Suspense>
+    </Canvas>
   );
-}
+};
+
+export default EarthCanvas;
